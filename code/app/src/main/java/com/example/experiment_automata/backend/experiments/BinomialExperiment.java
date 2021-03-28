d9049f10e0eff121fb5303e26dc34c501d085c21
Jordan <jdvandenbruel@gmail.com>
2021-03-28 15:29:07 -0600

    another attempt :/

--- code/app/src/main/java/com/example/experiment_automata/backend/experiments/BinomialExperiment.java
+++ code/app/src/main/java/com/example/experiment_automata/backend/experiments/BinomialExperiment.java
@@ -11,7 +11,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-
+//test for debugging
 /**
  * Role/Pattern:
  *      Holds the main data needed for a binomial experiment
